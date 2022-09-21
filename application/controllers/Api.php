343ea8b455d4000a5b853c164f3a84f5f1d495c0
octopuscart <octopuscartltd@gmail.com>
2022-09-21 13:41:43 +0530

    changes

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -11,7 +11,12 @@ class Api extends REST_Controller {
         $this->load->model('Order_model');
         $this->load->library('session');
         $this->checklogin = $this->session->userdata('logged_in');
-        $this->user_id = $this->session->userdata('logged_in')['login_id'];
+        $session_user = $this->session->userdata('logged_in');
+        if ($session_user) {
+            $this->user_id = $session_user['login_id'];
+        } else {
+            $this->user_id = 0;
+        }
     }
 
     public function index() {
